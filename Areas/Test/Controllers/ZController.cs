87446b8d09c917112843da8d9469adfe57e98b6c
Thomas Gielissen <thomas.gielissen@bosa.fgov.be>
2024-02-28 23:08:41 +0100

    Add project files.

---
+++ Areas/Test/Controllers/ZController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace WebApplication1.Areas.Test.Controllers
+{
+    public class ZController : Controller
+    {
+        public ActionResult Index(int? id)
+        {
+            var model = new ZModel()
+            {
+                Id = 1,
+                Items = new List<ZItem>()
+                {
+                    new ZItem() { Name = "Thomas", Selected = true},
+                    new ZItem() { Name = "Yassine", Selected = false}
+                }
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Update(ZModel model)
+        {
+            var x = model.Id;
+
+            return View("Index", model);
+        }
+    }
+
+    public class ZModel
+    {
+        public int Id { get; set; }
+
+        public List<ZItem> Items { get; set; }
+    }
+
+    public class ZItem
+    {
+        public string Name { get; set; }
+
+        public bool Selected { get; set; }
+    }
+}
